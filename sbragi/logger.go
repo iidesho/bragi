733652383343f92c6409ea36cd847b1ed1d97ab1
sindrebrurberg <sindre@brurberg.no>
2024-01-06 21:34:28 +0800

    adding error reporting

--- sbragi/logger.go
+++ sbragi/logger.go
@@ -12,25 +12,32 @@ import (
 var defaultLogger, _ = NewLogger(slog.NewTextHandler(os.Stdout, nil))
 
 type Logger interface {
-	Trace(msg string, args ...any)
-	Debug(msg string, args ...any)
-	Info(msg string, args ...any)
-	Notice(msg string, args ...any)
-	Warning(msg string, args ...any)
-	Error(msg string, args ...any)
-	Fatal(msg string, args ...any)
-	WithError(err error) Logger
+	ErrorLogger
+	WithError(err error) ErrorLogger
+	WithErrorFunc(errf func() error) ErrorLogger
 	WithoutEscalation() Logger
 	SetDefault()
 }
 
+type ErrorLogger interface {
+	Trace(msg string, args ...any) bool
+	Debug(msg string, args ...any) bool
+	Info(msg string, args ...any) bool
+	Notice(msg string, args ...any) bool
+	Warning(msg string, args ...any) bool
+	Error(msg string, args ...any) bool
+	Fatal(msg string, args ...any)
+}
+
 type logger struct {
-	handler  slog.Handler
-	slog     *slog.Logger
-	depth    int
-	ctx      context.Context
-	escalate bool
-	err      error
+	handler   slog.Handler
+	slog      *slog.Logger
+	depth     int
+	ctx       context.Context
+	escalate  bool
+	err       error
+	errf      func() error
+	withError bool
 }
 
 func NewLogger(handler slog.Handler) (logger, error) {
@@ -58,37 +65,45 @@ func (l logger) SetDefault() {
 	defaultLogger = l
 }
 
-func (l logger) Trace(msg string, args ...any) {
-	l.log(LevelTrace, msg, args...)
+func (l logger) Trace(msg string, args ...any) bool {
+	return l.log(LevelTrace, msg, args...)
 }
 
-func (l logger) Debug(msg string, args ...any) {
-	l.log(LevelDebug, msg, args...)
+func (l logger) Debug(msg string, args ...any) bool {
+	return l.log(LevelDebug, msg, args...)
 }
 
-func (l logger) Info(msg string, args ...any) {
-	l.log(LevelInfo, msg, args...)
+func (l logger) Info(msg string, args ...any) bool {
+	return l.log(LevelInfo, msg, args...)
 }
 
-func (l logger) Notice(msg string, args ...any) {
-	l.log(LevelNotice, msg, args...)
+func (l logger) Notice(msg string, args ...any) bool {
+	return l.log(LevelNotice, msg, args...)
 }
 
-func (l logger) Warning(msg string, args ...any) {
-	l.log(LevelWarning, msg, args...)
+func (l logger) Warning(msg string, args ...any) bool {
+	return l.log(LevelWarning, msg, args...)
 }
 
-func (l logger) Error(msg string, args ...any) {
-	l.log(LevelError, msg, args...)
+func (l logger) Error(msg string, args ...any) bool {
+	return l.log(LevelError, msg, args...)
 }
 
 func (l logger) Fatal(msg string, args ...any) {
-	l.log(LevelFatal, msg, args...)
-	panic(fmt.Sprint(msg, args))
+	if l.log(LevelFatal, msg, args...) || !l.withError {
+		panic(fmt.Sprint(msg, args))
+	}
 }
 
-func (l logger) WithError(err error) Logger {
+func (l logger) WithError(err error) ErrorLogger {
 	l.err = err
+	l.withError = true
+	//l.depth--
+	return l
+}
+func (l logger) WithErrorFunc(errf func() error) ErrorLogger {
+	l.errf = errf
+	l.withError = true
 	//l.depth--
 	return l
 }
@@ -98,31 +113,36 @@ func (l logger) WithoutEscalation() Logger {
 	return l
 }
 
-func Trace(msg string, args ...any) {
-	defaultLogger.Trace(msg, args...)
+func Trace(msg string, args ...any) bool {
+	return defaultLogger.Trace(msg, args...)
 }
-func Debug(msg string, args ...any) {
-	defaultLogger.Debug(msg, args...)
+func Debug(msg string, args ...any) bool {
+	return defaultLogger.Debug(msg, args...)
 }
-func Info(msg string, args ...any) {
-	defaultLogger.Info(msg, args...)
+func Info(msg string, args ...any) bool {
+	return defaultLogger.Info(msg, args...)
 }
-func Notice(msg string, args ...any) {
-	defaultLogger.Notice(msg, args...)
+func Notice(msg string, args ...any) bool {
+	return defaultLogger.Notice(msg, args...)
 }
-func Warning(msg string, args ...any) {
-	defaultLogger.Warning(msg, args...)
+func Warning(msg string, args ...any) bool {
+	return defaultLogger.Warning(msg, args...)
 }
-func Error(msg string, args ...any) {
-	defaultLogger.Error(msg, args...)
+func Error(msg string, args ...any) bool {
+	return defaultLogger.Error(msg, args...)
 }
 func Fatal(msg string, args ...any) {
 	defaultLogger.Fatal(msg, args...)
 }
-func WithError(err error) Logger {
+func WithError(err error) ErrorLogger {
 	l := defaultLogger
 	l.depth--
 	return l.WithError(err)
+}
+func WithErrorFunc(errf func() error) ErrorLogger {
+	l := defaultLogger
+	l.depth--
+	return l.WithErrorFunc(errf)
 }
 func WithoutEscalation() Logger {
 	l := defaultLogger
@@ -133,15 +153,26 @@ func WithoutEscalation() Logger {
 // log is the low-level logging method for methods that take ...any.
 // It must always be called directly by an exported logging method
 // or function, because it uses a fixed call depth to obtain the pc.
-func (l logger) log(level slog.Level, msg string, args ...any) {
-	if l.escalate && l.err != nil && level < LevelError {
-		level = LevelError
+func (l logger) log(level slog.Level, msg string, args ...any) (loggedError bool) {
+	//loggedError = l.err != nil
+	if l.errf != nil {
+		l.err = l.errf()
+	}
+	if l.err != nil {
+		if l.escalate && level < LevelError {
+			level = LevelError
+		}
+	} else {
+		if level >= LevelError && l.withError {
+			return false //Return early if level is error and there was no error
+		}
 	}
 	if !l.handler.Enabled(l.ctx, level) {
-		return
+		return false
 	}
 	if l.err != nil {
 		args = append([]any{"error", l.err}, args...)
+		loggedError = true
 	}
 	var pc uintptr
 	var pcs [1]uintptr
@@ -151,4 +182,5 @@ func (l logger) log(level slog.Level, msg string, args ...any) {
 	r := slog.NewRecord(time.Now(), level, msg, pc)
 	r.Add(args...)
 	_ = l.handler.Handle(l.ctx, r)
+	return
 }
